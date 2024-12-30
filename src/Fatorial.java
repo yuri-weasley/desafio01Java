1b7be8b6a03e52ea80dc6d0866d610b11e443aa9
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-12-30 08:22:51 -0400

    Minhas resoluções elaboradas dos desdafios propostos no curso "Java: Criando sua primeira aplicação" Com a instrutora Jacqueline Oliveira e Paulo Silveira.

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Fatorial {
     public static void main(String[] args) {
-        System.out.println("Bem-vindo(a) à calculadora de fatoriais!");
+        System.out.println("Bem-vindo(a) à calculadora de Fatorial!");
         System.out.println("Digite um número e calcularemos seu fatorial:");
         Scanner leitor = new Scanner(System.in);
         int numero = leitor.nextInt();
-        for(int i = numero - 1; i >= 1; i--) {
-            int fatorial = 1;
-            fatorial = numero * i * fatorial;
-            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        int fatorial = 1;
+
+        for(int i = 1; i <= numero; i++) {
+            fatorial *= i;
         }
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
         /*
             num! = num.(num - 1).(num - 2).(num - 3).....(num - (num - 1))
             7! = 7.6.5.4.....1
