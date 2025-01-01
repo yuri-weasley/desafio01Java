b6a48c981d5c62aecff6422cbb2a7706dab25953
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-01-01 10:00:11 -0400

    Minhas resoluções elaboradas dos desdafios propostos no curso "Java: Criando sua primeira aplicação" Com a instrutora Jacqueline Oliveira e Paulo Silveira.

--- src/Desafio.java
+++ src/Desafio.java
@@ -5,6 +5,7 @@ public class Desafio extends Conta {
         Conta minhaConta = new Conta();
         minhaConta.setNome("Yuri Ferreira");
         minhaConta.setTipo("Poupança");
+        int opcao = 0;
 
         System.out.println("------- Dados iniciais do cliente -------");
         System.out.println("Nome: " + minhaConta.getNome());
@@ -12,38 +13,34 @@ public class Desafio extends Conta {
         System.out.println("Saldo atual: R$ " + String.format("%.2f", minhaConta.getSaldo()));
         System.out.println("-----------------------------------------");
 
-        System.out.println("Operações:");
-        System.out.println("1 - Consultar saldo");
-        System.out.println("2 - Receber valor");
-        System.out.println("3 - Transferir valor");
-        System.out.println("4 - Sair");
-        System.out.println("Digite a opção desejada:");
-        Scanner leitor = new Scanner(System.in);
-        int opcao = leitor.nextInt();
+        String menu = """
+                Digite sua opção:
+                1 - Consultar saldo
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """;
 
-        while(opcao >= 1 && opcao <= 4) {
+        Scanner leitor = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitor.nextInt();
+
             if (opcao == 1) {
                 System.out.println("Seu saldo atual é: R$ " + String.format("%.2f", minhaConta.getSaldo()));
-                break;
             } else if (opcao == 2) {
                 System.out.println("Digite o valor a ser depositado em R$: ");
                 double valor = leitor.nextDouble();
                 minhaConta.depositar(valor);
                 System.out.println("Novo saldo: R$ " + String.format("%.2f", +minhaConta.getSaldo()));
-                break;
             } else if (opcao == 3) {
                 System.out.println("Digite o valor a ser transferido em R$: ");
                 double valor = leitor.nextDouble();
                 minhaConta.transferir(valor);
-                break;
-            } else if (opcao == 4) {
-                System.out.println("Encerrando o programa. Até a próxima!");
-                break;
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida.");
             }
         }
-        if(opcao < 1 || opcao > 4) {
-            System.out.println("Opção digitada inválida. Tente novamente.");
-        }
-
     }
 }
